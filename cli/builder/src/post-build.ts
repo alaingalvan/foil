3c070a36f6d6bbc62080a61c0221b48c2e802fc8
Alain Galvan <hi@alain.xyz>
2024-09-09 17:13:24 -0400

    Added post-build workflow to Typescript portions of Foil
    Foil depends on Node.js and Typescript to resolve imports and build foil modules with Webpack. Past distributions failed to include `tsconfig.json` files and at least 1 `.ts` file, as required by Typescript, the post-build script aims to always properly compile Typescript to include '.js' extensions and copy needed files to execute node as a subprocess.

--- cli/builder/src/post-build.ts
+++ cli/builder/src/post-build.ts
@@ -1,8 +1,10 @@
 import {
   readFile as readFileCallback,
   writeFile as writeFileCallback,
+  mkdir as mkdirCallback,
+  cp as cpCallback,
 } from "fs";
-import { join } from "path";
+import { join, resolve } from "path";
 import { promisify } from "util";
 import Find from "find";
 const { fileSync } = Find;
@@ -10,23 +12,58 @@ import { cwd } from "process";
 
 const readFile = promisify(readFileCallback);
 const writeFile = promisify(writeFileCallback);
+const mkdir = promisify(mkdirCallback);
+const cp: any = promisify(cpCallback);
 
-// Find all relative imports in dist/*/*.js files, append .js extension if missing.
 async function postBuild() {
-  let foundJsFiles = fileSync(/\.js$"/, join(cwd(), "dist"));
-
-  // Enqueue tasks:
+  // Find all relative imports in dist/*/*.js files, append .js extension if missing.
+  let distPath = join(cwd(), "dist");
+  let foundJsFiles = fileSync(/\.js$/, distPath);
+  console.log(
+    `🔨 Found ${foundJsFiles.length} files in ${distPath}, fixing imports to include .js.`
+  );
   let threads = [];
   for (let foundJsFile of foundJsFiles) {
-    let jsFile = await readFile(foundJsFile).toString();
-    jsFile.replaceAll(/from ("|')(\.\/.+)("|')/, "from '$2.js'");
+    let jsFile = (await readFile(foundJsFile)).toString();
+    jsFile.replaceAll(/(import\s*.*)("|')(\.\/.*)("|')/g, "$1$2$3.js$4");
     threads.push(writeFile(foundJsFile, jsFile));
   }
+  do {
+    await threads.pop().catch((e) => console.log(e));
+  } while (threads.length > 0);
 
-  // Clear queue:
-  for (let t of threads) {
-    await t;
+  // Copy dist to release folder:
+  let releaseTargetPath = resolve(join(cwd(), "..", "target"));
+  let releasePaths = [
+    join(releaseTargetPath, "release", "builder"),
+    join(releaseTargetPath, "debug", "builder"),
+  ];
+  for (let releasePath of releasePaths) {
+    await mkdir(releasePath).catch((e) => {});
+    threads.push(cp(distPath, join(releasePath, "dist"), { recursive: true }));
+    threads.push(
+      cp(join(cwd(), "src"), join(releasePath, "src"), { recursive: true })
+    );
+    threads.push(
+      cp(join(cwd(), "package.json"), join(releasePath, "package.json"))
+    );
+    threads.push(
+      cp(
+        join(cwd(), "package-lock.json"),
+        join(releasePath, "package-lock.json")
+      )
+    );
+    threads.push(
+      cp(join(cwd(), "tsconfig.json"), join(releasePath, "tsconfig.json"))
+    );
+    console.log(
+      "📄 Copied 'dist/', 'src/', 'package.json', 'package-lock.json', 'tsconfig.json' to:\n" +
+        releasePath
+    );
   }
+  do {
+    await threads.pop().catch((e) => console.log(e));
+  } while (threads.length > 0);
 }
 
 postBuild();
